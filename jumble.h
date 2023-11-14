e2f861ebbea4d8a1dffc85e8dc5adb5399f30f89
Conor Spalvieri <conorspalvieri1@gmail.com>
2023-11-13 23:24:33 -0500

    starting header file

---
+++ jumble.h
@@ -0,0 +1,20 @@
+using namespace std;
+
+
+class BadJumbleException{
+    public:
+        BadJumbleException(const string& message);
+        string what() const;
+    private:
+        string message;
+};
+
+
+class JumblePuzzle{
+    
+
+
+
+
+
+};
